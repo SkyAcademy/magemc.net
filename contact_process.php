9a1494f404ba42b410667920200017a3ea29fccb
ConjureCraft <42539420+SkyAcademy@users.noreply.github.com>
2019-10-30 13:07:54 +0100

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "spn10@spondonit.com";
+    $to = "niels.leemans1@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
